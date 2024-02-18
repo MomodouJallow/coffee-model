1706117ee85e7f42b696109a304f89d00da1164a
BANADDA MUBARAKA <83466862+BANADDA@users.noreply.github.com>
2024-02-18 23:13:57 +0300

    modified:   app.py

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
 import io
-
 import numpy as np
 import tensorflow as tf
 import uvicorn
-from fastapi import FastAPI, File, UploadFile
-from fastapi.encoders import jsonable_encoder
+from fastapi import FastAPI, UploadFile, File
 from PIL import Image
 
 # Define your class labels
-class_labels = ["miner", "rust", "phome"]  
+class_labels = ['miner', 'nodisease', 'phoma', 'rust']
 
 # Load TFLite model and allocate tensors
 interpreter = tf.lite.Interpreter(model_path="models/converted_model.tflite")
@@ -18,17 +16,13 @@ interpreter.allocate_tensors()
 input_details = interpreter.get_input_details()
 output_details = interpreter.get_output_details()
 
-# Print input shape
-print("Input shape:", input_details[0]['shape'])
-
 app = FastAPI()
 
 @app.post("/predict/")
-async def predict_image(file: UploadFile = File(...)):
+async def predict_image(file: bytes = File(...)):
     try:
         # Load and preprocess the image
-        contents = await file.read()
-        image = Image.open(io.BytesIO(contents)).convert("RGB")
+        image = Image.open(io.BytesIO(file)).convert("RGB")
         input_size = (input_details[0]['shape'][2], input_details[0]['shape'][1])
         image.thumbnail(input_size, Image.LANCZOS)
         image = np.array(image.resize((input_size[0], input_size[1]), Image.LANCZOS), dtype=np.float32)
@@ -44,7 +38,6 @@ async def predict_image(file: UploadFile = File(...)):
         output_data = interpreter.get_tensor(output_details[0]['index'])
         predicted_class_index = np.argmax(output_data)
         predicted_class = class_labels[predicted_class_index]
-        #Hello
 
         result = {
             "predicted_class": predicted_class,
@@ -53,7 +46,7 @@ async def predict_image(file: UploadFile = File(...)):
     except Exception as e:
         result = {"Error": f"Failed to predict image: {str(e)}"}
 
-    return jsonable_encoder(result)
+    return result
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
